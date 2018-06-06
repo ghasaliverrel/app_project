f224938ce6800a817c0e7ccf1b0e15fce04d7c63
ghasaliverrel <vghasali@gmail.com>
2018-06-07 01:59:37 +0700

    Initial Commit

---
+++ application/views/panels/productpanel_page.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+	<title>FoodSteps - Explore Page</title>
+	<?php $this->load->view('header')?>
+</head>
+<body>
+	<div class="heading">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  	<a class="brand-name" href="#">FoodSteps</a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  	</button>
+		  	<div class="collapse navbar-collapse" id="navbarNav">
+		    	<!-- Right Float Link -->
+		    	 <ul class="navbar-nav ml-auto brand-links">
+				    <li class="nav-item brand-link">
+			        	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			      	</li>
+			      	<li class="nav-item brand-link">
+			        	<a class="nav-link" href="#">Explore</a>
+			      	</li>
+			      	<li class="nav-item brand-link">
+			        	<a class="nav-link" href="#">About</a>
+			      	</li>
+				  </ul>
+		  	</div>
+		</nav>
+	</div>
+
+	<div class="content">
+		<div class="content-bg row">
+			<!-- <div class="row"> -->
+				<div class="col-sm-10">
+					<div class="row">
+						<div class="content-boxs col-sm-3 mt-2 ml-1">
+							<div class="card">
+							  	<img class="card-img-top" src="<?=base_url()?>assets/img/product-streetfood.jpg" alt="Card image cap">
+							  	<div class="card-body">
+							    	<h5 class="card-title">Street Food</h5>
+							    	<p class="card-text">Street food is ready-to-eat food or drink sold by a hawker, or vendor, in a street or other public place, such as at a market or fair. It is often sold from a portable food booth, food cart, or food truck and meant for immediate consumption</p>
+							  	</div>
+							  	<div class="card-body">
+							    	<a href="#" class="card-link">Find nearest place in your area! >></a>
+							  	</div>
+							</div>
+						</div>
+
+						<div class="content-boxs col-sm-3 mt-2 ml-1">
+							<div class="card">
+								<div class="card-image">
+									<img class="card-img-top" src="<?=base_url()?>assets/img/product-restaurant.jpg" alt="Card image cap">
+								</div>
+							  	<div class="card-body">
+							    	<h5 class="card-title">Restaurant</h5>
+							    	<p class="card-text">A restaurant or an eatery, is a business which prepares and serves food and drinks to customers in exchange for money. Meals are generally served and eaten on the premises, but many restaurants also offer take-out and food delivery services</p>
+							  	</div>
+							  	<div class="card-body">
+							    	<a href="#" class="card-link">Find nearest place in your area! >></a>
+							  	</div>
+							</div>
+						</div>
+
+						<div class="content-boxs col-sm-3 mt-2 ml-1">
+							<div class="card">
+							  	<img class="card-img-top" src="<?=base_url()?>assets/img/product-breakfast.jpg" alt="Card image cap">
+							  	<div class="card-body">
+							    	<h5 class="card-title">Breakfast</h5>
+							    	<p class="card-text">Breakfast is the first meal of a day, most often eaten in the early morning before undertaking the day's work.</p>
+							  	</div>
+							  	<div class="card-body">
+							    	<a href="#" class="card-link">Find nearest place in your area! >></a>
+							  	</div>
+							</div>
+						</div>
+
+						<div class="content-boxs col-sm-3 mt-2 ml-1">
+							<div class="card">
+							  	<img class="card-img-top" src="<?=base_url()?>assets/img/product-bbq.jpg" alt="Card image cap">
+							  	<div class="card-body">
+							    	<h5 class="card-title">Grilled Food</h5>
+							    	<p class="card-text">Barbecue or barbeque (informally BBQ or barbie) is both a cooking method and an apparatus/machine. Barbecuing is done slowly over low, indirect heat and the food is flavored by the smoking process</p>
+							  	</div>
+							  	<div class="card-body">
+							    	<a href="#" class="card-link">Find nearest place in your area! >></a>
+							  	</div>
+							</div>
+						</div>
+
+						<div class="content-boxs col-sm-3 mt-2 ml-1">
+							<div class="card">
+							  	<img class="card-img-top" src="<?=base_url()?>assets/img/product-seafood.jpg" alt="Card image cap">
+							  	<div class="card-body">
+							    	<h5 class="card-title">Fresh Seafood</h5>
+							    	<p class="card-text">Seafood is any form of sea life regarded as food by humans. Seafood prominently includes fish and shellfish. Shellfish include various species of molluscs, crustaceans, and echinoderms</p>
+							  	</div>
+							  	<div class="card-body">
+							    	<a href="#" class="card-link">Find nearest place in your area! >></a>
+							  	</div>
+							</div>
+						</div>
+
+						<div class="content-boxs col-sm-3 mt-2 ml-1">
+							<div class="card">
+							  	<img class="card-img-top" src="<?=base_url()?>assets/img/product-snack.jpg" alt="Card image cap">
+							  	<div class="card-body">
+							    	<h5 class="card-title">Snack</h5>
+							    	<p class="card-text">A snack is a portion of food, smaller than a regular meal, generally eaten between meals. Snacks come in a variety of forms including packaged snack foods and other processed foods</p>
+							  	</div>
+							  	<div class="card-body">
+							    	<a href="#" class="card-link">Find nearest place in your area! >></a>
+							  	</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-2">
+					
+				</div>
+
+			<!-- </div> -->
+		</div>
+	</div>
+</body>
+</html>
---
+++ application/views/panels/restaurantlist_page.php
@@ -0,0 +1,114 @@
+<html>
+<head>
+	<title>FoodSteps - Restaurant List</title>
+	<?php $this->load->view('header')?>
+</head>
+<body>
+	<div class="heading">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  	<a class="brand-name" href="#">FoodSteps</a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  	</button>
+		  	<div class="collapse navbar-collapse" id="navbarNav">
+		    	<!-- Right Float Link -->
+		    	 <ul class="navbar-nav ml-auto brand-links">
+				    <li class="nav-item brand-link">
+			        	<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+			      	</li>
+			      	<li class="nav-item brand-link">
+			        	<a class="nav-link" href="#">Explore</a>
+			      	</li>
+			      	<li class="nav-item brand-link">
+			        	<a class="nav-link" href="#">About</a>
+			      	</li>
+				  </ul>
+		  	</div>
+		</nav>
+	</div>
+
+	<div class="content">
+		<div class="content-bg">
+			<div class="item active left row">
+      			<div class="col-md-6 col-sm-6 left-images">
+        			<img src="<?= base_url()?>assets/img/reserve.jpg" class="img-responsive">
+      			</div>
+      			<div class="col-md-6 col-sm-6 details-text">
+        			<div class="cor">
+          				<h2>Joyful party</h2>
+          				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eos suscipit earum voluptas aliquam recusandae, quae iure adipisci, inventore quia, quos delectus quaerat praesentium id expedita nihil illo accusantium, tempora.</p>
+          				<address>
+                          	<strong>Place: </strong>
+                          	1612 Collins Str, Victoria 8007
+                          	<br>
+                          	<strong>Time: </strong>
+                          	07:30pm
+                    	</address>
+          				<a class="btn btn-imfo btn-read-more" href="events-details.html">Read more</a>
+        			</div>
+      			</div>
+    		</div>
+
+    		<div class="item active left row">
+      			<div class="col-md-6 col-sm-6 left-images">
+        			<img src="<?= base_url()?>assets/img/reserve.jpg" class="img-responsive">
+      			</div>
+      			<div class="col-md-6 col-sm-6 details-text">
+        			<div class="cor">
+          				<h2>Joyful party</h2>
+          				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eos suscipit earum voluptas aliquam recusandae, quae iure adipisci, inventore quia, quos delectus quaerat praesentium id expedita nihil illo accusantium, tempora.</p>
+          				<address>
+                          	<strong>Place: </strong>
+                          	1612 Collins Str, Victoria 8007
+                          	<br>
+                          	<strong>Time: </strong>
+                          	07:30pm
+                    	</address>
+          				<a class="btn btn-imfo btn-read-more" href="events-details.html">Read more</a>
+        			</div>
+      			</div>
+    		</div>
+
+    		<div class="item active left row">
+      			<div class="col-md-6 col-sm-6 left-images">
+        			<img src="<?= base_url()?>assets/img/reserve.jpg" class="img-responsive">
+      			</div>
+      			<div class="col-md-6 col-sm-6 details-text">
+        			<div class="cor">
+          				<h2>Joyful party</h2>
+          				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eos suscipit earum voluptas aliquam recusandae, quae iure adipisci, inventore quia, quos delectus quaerat praesentium id expedita nihil illo accusantium, tempora.</p>
+          				<address>
+                          	<strong>Place: </strong>
+                          	1612 Collins Str, Victoria 8007
+                          	<br>
+                          	<strong>Time: </strong>
+                          	07:30pm
+                    	</address>
+          				<a class="btn btn-imfo btn-read-more" href="events-details.html">Read more</a>
+        			</div>
+      			</div>
+    		</div>
+
+    		<div class="item active left row">
+      			<div class="col-md-6 col-sm-6 left-images">
+        			<img src="<?= base_url()?>assets/img/reserve.jpg" class="img-responsive">
+      			</div>
+      			<div class="col-md-6 col-sm-6 details-text">
+        			<div class="cor">
+          				<h2>Joyful party</h2>
+          				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore eos suscipit earum voluptas aliquam recusandae, quae iure adipisci, inventore quia, quos delectus quaerat praesentium id expedita nihil illo accusantium, tempora.</p>
+          				<address>
+                          	<strong>Place: </strong>
+                          	1612 Collins Str, Victoria 8007
+                          	<br>
+                          	<strong>Time: </strong>
+                          	07:30pm
+                    	</address>
+          				<a class="btn btn-imfo btn-read-more" href="events-details.html">Read more</a>
+        			</div>
+      			</div>
+    		</div>
+		</div>
+	</div>
+</body>
+</html>
